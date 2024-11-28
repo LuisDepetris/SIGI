84a771f0ec617e4db2ad7d4c5e7c06c88dda1879
SantiagoSarasola <santisarasola@yahoo.com.ar>
2024-11-28 00:54:21 -0300

    Se agrega fix sobre las validaciones de la forma de pago

--- Back/middlewares/validarAtributosFormasPago.js
+++ Back/middlewares/validarAtributosFormasPago.js
@@ -6,11 +6,17 @@ const validarAtributosFormasPago = () => [
     .notEmpty()
     .withMessage("No se puede cargar una forma de pago vacia")
     .bail()
-    .matches(/^[a-zA-Z\s]+$/)
+    .isAlphanumeric('es-ES',{ignore:' '})
+    .withMessage("La forma de pago debe ser alfanumérica")
     .bail()
-    .withMessage("La forma de pago solo debe contener letras")
     .isLength({ min: 2 })
     .withMessage("La forma de pago debe tener al menos 2 caracteres")
+    .bail()
+    .isLength({ max: 50 })
+    .withMessage("La forma de pago no puede tener más de 50 caracteres")
+    .bail()
+    .matches(/[a-zA-Z]/)
+    .withMessage("La forma de pago debe contener al menos una letra"),
 ];
 
 export default validarAtributosFormasPago;
