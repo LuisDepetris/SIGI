01795234b563eb0b86afc8d4fd0bf35b3329bee3
Agustin Zalazar <agustin.zalazar12@gmail.com>
2024-11-28 17:48:22 -0300

    fix editar-venta

--- Front/src/pages/EditarProductoVenta.jsx
+++ Front/src/pages/EditarProductoVenta.jsx
@@ -232,6 +232,9 @@ function EditarProductoVentas() {
 
   const elegirMedioPago = (e) => {
     const idActual = parseInt(e.target.value, 10);
+    if (idActual === -1) {
+      navigate("/GestionFormPagos");
+    }
     if (isNaN(idActual)) {
       // Si el valor no es un número válido, restablece el estado
       setFormaPagoSeleccionada("");
